96090fcefadf6f2591cc416243d0f60bed13bd8c
TASagent <TASagent.Streams@gmail.com>
2024-11-23 10:49:26 -0600

    Solved 9 and 10 WIP

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -4,7 +4,9 @@ Console.WriteLine("Day 09 - Mirage Maintenance");
 Console.WriteLine("Star 1");
 Console.WriteLine();
 
-string[] lines = File.ReadAllLines(inputFile);
+IEnumerable<string> lines = File.ReadAllLines(inputFile).Where(x=>x.Length > 0);
+//IEnumerable<string> lines = new List<string>() { "10 13 16 21 30 45" };
+
 
 List<long> newElements = new List<long>();
 
@@ -13,15 +15,20 @@ foreach (string line in lines)
     newElements.Add(FindNextElement(line));
 }
 
-Console.WriteLine($"The answer is: {value}");
+Console.WriteLine($"The answer is: {newElements.Sum()}");
 
 Console.WriteLine();
 Console.WriteLine("Star 2");
 Console.WriteLine();
 
-int value2 = 0;
+List<long> newPreviousElements = new List<long>();
 
-Console.WriteLine($"The answer is: {value2}");
+foreach (string line in lines)
+{
+    newPreviousElements.Add(FindPreviousElement(line));
+}
+
+Console.WriteLine($"The answer is: {newPreviousElements.Sum()}");
 
 Console.WriteLine();
 Console.ReadKey();
@@ -30,5 +37,65 @@ Console.ReadKey();
 //Approach: Iterate, pushing old lists into stack, until elements are all 0
 long FindNextElement(string line)
 {
-    IEnumerable<>
+    Stack<List<long>> sequenceStack = new Stack<List<long>>();
+
+    List<long> sequence = line.Split(' ').Select(long.Parse).ToList();
+
+    sequenceStack.Push(sequence);
+
+    while (!sequence.All(x => x == 0))
+    {
+        List<long> newSequence = new List<long>();
+
+        for (int i = 0; i < sequence.Count - 1; i++)
+        {
+            newSequence.Add(sequence[i + 1] - sequence[i]);
+        }
+
+        sequenceStack.Push(newSequence);
+
+        sequence = newSequence;
+    }
+
+    long result = 0;
+
+    while (sequenceStack.Count > 0)
+    {
+        result += sequenceStack.Pop()[^1];
+    }
+
+    return result;
+}
+
+//Approach: Iterate, pushing old lists into stack, until elements are all 0
+long FindPreviousElement(string line)
+{
+    Stack<List<long>> sequenceStack = new Stack<List<long>>();
+
+    List<long> sequence = line.Split(' ').Select(long.Parse).ToList();
+
+    sequenceStack.Push(sequence);
+
+    while (!sequence.All(x => x == 0))
+    {
+        List<long> newSequence = new List<long>();
+
+        for (int i = 0; i < sequence.Count - 1; i++)
+        {
+            newSequence.Add(sequence[i + 1] - sequence[i]);
+        }
+
+        sequenceStack.Push(newSequence);
+
+        sequence = newSequence;
+    }
+
+    long result = 0;
+
+    while (sequenceStack.Count > 0)
+    {
+        result = sequenceStack.Pop()[0] - result;
+    }
+
+    return result;
 }
--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,23 +1,232 @@
-﻿const string inputFile = @"../../../../input10.txt";
+﻿using AoCTools;
 
-Console.WriteLine("Day 10 - ???");
+const string inputFile = @"../../../../input10.txt";
+
+Console.WriteLine("Day 10 - Pipe Maze");
 Console.WriteLine("Star 1");
 Console.WriteLine();
 
-//string[] lines = File.ReadAllLines(inputFile);
-//string line = File.ReadAllText(inputFile);
+string[] lines = File.ReadAllLines(inputFile);
 
-int value = 0;
+Point2D startingPoint = FindStartingPoint();
 
-Console.WriteLine($"The answer is: {value}");
+Dictionary<Point2D, int> distanceMap = new Dictionary<Point2D, int>();
+distanceMap[startingPoint] = 0;
+
+Queue<Point2D> pipesToCheck = new Queue<Point2D>();
+pipesToCheck.Enqueue(startingPoint);
+
+while (pipesToCheck.Count > 0)
+{
+    Point2D checkingPipe = pipesToCheck.Dequeue();
+    int newDistance = distanceMap[checkingPipe] + 1;
+
+    for (Direction direction = 0; direction < Direction.MAX; direction++)
+    {
+        Point2D newPosition = ShiftPosition(checkingPipe, direction);
+        if (CanTravel(checkingPipe, direction) && (!distanceMap.ContainsKey(newPosition) || distanceMap[newPosition] > newDistance))
+        {
+            distanceMap[newPosition] = newDistance;
+            pipesToCheck.Enqueue(newPosition);
+        }
+    }
+}
+
+
+Console.WriteLine($"The answer is: {distanceMap.Values.Max()}");
 
 Console.WriteLine();
 Console.WriteLine("Star 2");
 Console.WriteLine();
 
-int value2 = 0;
 
-Console.WriteLine($"The answer is: {value2}");
+Point2D UpperLeft = (distanceMap.Keys.Min(x => x.x), distanceMap.Keys.Min(x => x.y));
+Point2D BottomRight = (distanceMap.Keys.Max(x => x.x), distanceMap.Keys.Max(x => x.y));
+
+
+Dictionary<Point2D, char> printOutput = new Dictionary<Point2D, char>();
+
+foreach (Point2D pipe in distanceMap.Keys)
+{
+    printOutput[pipe - UpperLeft] = lines[pipe.y][pipe.x];
+}
+
+
+int enclosedCount = 0;
+
+for (int x = UpperLeft.x + 1; x < BottomRight.x; x++)
+{
+    for (int y = UpperLeft.y + 1; y < BottomRight.y; y++)
+    {
+        if (distanceMap.ContainsKey((x, y))) continue;
+
+        //Found an empty spot
+
+        int pipeCount = 0;
+        for (int checkX = UpperLeft.x; checkX < x; checkX++)
+        {
+            if (distanceMap.ContainsKey((checkX, y)) && lines[y][checkX] != '-') pipeCount++;
+        }
+
+        if (pipeCount % 2 == 0) continue;
+
+        pipeCount = 0;
+        for (int checkY = UpperLeft.y; checkY < y; checkY++)
+        {
+            if (distanceMap.ContainsKey((x, checkY)) && lines[checkY][x] != '|') pipeCount++;
+        }
+
+        if (pipeCount % 2 == 0) continue;
+
+        enclosedCount++;
+        printOutput[(x, y) - UpperLeft] = 'I';
+    }
+}
+
+Console.WriteLine($"The answer is: {enclosedCount}");
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Grid:");
+Console.WriteLine();
+Console.ReadKey();
+Console.WriteLine();
+
+
+for (int y = 0; y < BottomRight.y - UpperLeft.y + 1; y++)
+{
+    for (int x = 0; x < BottomRight.x - UpperLeft.x + 1; x++)
+    {
+        if (printOutput.ContainsKey((x, y)))
+        {
+            if (printOutput[(x, y)] == 'I')
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.Write(' ');
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            else
+            {
+                Console.Write(printOutput[(x, y)]);
+            }
+        }
+        else
+        {
+            Console.Write(' ');
+        }
+    }
+    Console.WriteLine();
+}
+
+
+
+Console.WriteLine();
+
 
 Console.WriteLine();
 Console.ReadKey();
+
+
+bool CanTravel(Point2D position, Direction direction)
+{
+    Point2D newPosition = ShiftPosition(position, direction);
+
+    if (newPosition.y < 0 || newPosition.y >= lines.Length) return false;
+    if (newPosition.x < 0 || newPosition.x >= lines[newPosition.y].Length) return false;
+
+    char current = lines[position.y][position.x];
+    char newPipe = lines[newPosition.y][newPosition.x];
+
+    return PipeSupportsDirection(current, direction) && PipeSupportsDirection(newPipe, direction.Invert());
+}
+
+Point2D ShiftPosition(Point2D position, Direction direction) => direction switch
+{
+    Direction.Up => position + (0, -1),
+    Direction.Down => position + (0, 1),
+    Direction.Left => position + (-1, 0),
+    Direction.Right => position + (1, 0),
+    _ => throw new Exception(),
+};
+
+bool PipeSupportsDirection(char pipe, Direction direction)
+{
+    switch (direction)
+    {
+        case Direction.Up:
+            return pipe switch
+            {
+                'L' => true,
+                'J' => true,
+                '|' => true,
+                'S' => true,
+                _ => false
+            };
+        case Direction.Down:
+            return pipe switch
+            {
+                '7' => true,
+                'F' => true,
+                '|' => true,
+                'S' => true,
+                _ => false
+            };
+        case Direction.Left:
+            return pipe switch
+            {
+                '7' => true,
+                'J' => true,
+                '-' => true,
+                'S' => true,
+                _ => false
+            };
+        case Direction.Right:
+            return pipe switch
+            {
+                'L' => true,
+                'F' => true,
+                '-' => true,
+                'S' => true,
+                _ => false
+            };
+        default:
+            throw new Exception();
+    }
+}
+
+Point2D FindStartingPoint()
+{
+    for (int y = 0; y < lines.Length; y++)
+    {
+        for (int x = 0; x < lines[y].Length; x++)
+        {
+            if (lines[y][x] == 'S')
+            {
+                return new Point2D(x, y);
+            }
+        }
+    }
+
+    throw new Exception();
+}
+
+public enum Direction
+{
+    Up = 0,
+    Down = 1,
+    Left = 2,
+    Right = 3,
+    MAX
+}
+
+
+public static class ExtensionMethods
+{
+    public static Direction Invert(this Direction direction) => direction switch
+    {
+        Direction.Up => Direction.Down,
+        Direction.Left => Direction.Right,
+        Direction.Right => Direction.Left,
+        Direction.Down => Direction.Up,
+        _ => throw new Exception()
+    };
+}
